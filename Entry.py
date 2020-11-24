9bfb825efd4fadc9d37ea89f360ae6b0f2d6fc2c
PHimmel <peterhimmelman@yahoo.com>
2020-11-24 13:35:34 -0500

    changed import specifics

--- Entry.py
+++ Entry.py
@@ -9,10 +9,10 @@ Now everytime you login you will have a second layer of protection.
 
 """
 
-import os
+from os import popen
 from time import sleep
 
-# user configuration 
+# user configuration
 chances = 3
 password = 'password'
 # end user configuration
@@ -34,10 +34,9 @@ class Entry:
         sleep(3)
 
         # kills ALL open ssh processes running on host/server machine
-        os.popen('pkill ssh')
+        popen('pkill ssh')
 
 
 # program execution
 run = Entry(password, chances)
 run.attempt()
-
